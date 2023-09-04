a7e9484aecabe2a804d380f452eb1eaf3c0f888b
rifat977 <itscrifat5147@gmail.com>
2023-09-05 03:31:40 +0600

    update details

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -4,10 +4,10 @@ const config = {
   github: {
     username: 'rifat977', // Your GitHub org/user name. (Required)
     sortBy: 'updated', // stars | updated
-    limit: 6, // How many projects to display.
+    limit: 8, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: ['Rifat977'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
