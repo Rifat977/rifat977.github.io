b7622e0cd6e4192c477c8c37aaa9054db7071196
rifat977 <itscrifat5147@gmail.com>
2023-09-26 14:06:31 +0600

    blog place change

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -105,9 +105,9 @@ const config = {
   ],
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
-    source: 'medium', // medium | dev
+    source: 'dev', // medium | dev
     username: 'rifat977', // to hide blog section, keep it empty
-    limit: 2, // How many posts to display. Max is 10.
+    limit: 10, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
